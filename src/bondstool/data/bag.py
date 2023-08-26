3320e0103391fb50e30db24d0669a902b570f08d
Bohdan Yakovenko <byakovenko@kse.org.ua>
2023-08-26 17:36:18 +0300

    Analyse bag excel + format bag table

--- src/bondstool/data/bag.py
+++ src/bondstool/data/bag.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from bondstool.utils import split_dataframe
 
 OVDP_BAG_PATH = "data/ovdp_bag_31.05.23.xlsx"
 ISIN_PREFIX = "UA4000"
@@ -8,9 +9,23 @@ def read_bag_info():
 
     bag = pd.read_excel(OVDP_BAG_PATH)
     bag = bag.loc[1:10].reset_index(drop=True)
-    bag["ISIN"] = ISIN_PREFIX + bag["ISIN"].astype("str")
+    # bag["ISIN"] = ISIN_PREFIX + bag["ISIN"].astype("str")
 
-    map_headings = {"Кіл-ть в портфелі": "quantity"}
+    bag = bag.drop(
+        columns=[
+            "Тип",
+            "Дата погашеня",
+            "Очікувана сума погашення",
+            "Інвестиційний результат очікуваний без податку на прибуток",
+            "Інвестиційний результат очікуваний за мінусом ПнПр",
+        ]
+    )
+
+    map_headings = {
+        "Кіл-ть в портфелі": "quantity",
+        "Загальна сума придбання": "expenditure",
+        "Податок на прибуток ЮО (ПнПр)": "tax",
+    }
 
     bag = bag.rename(columns=map_headings)
 
@@ -20,10 +35,57 @@ def read_bag_info():
 def merge_bonds_info(bag: pd.DataFrame, bonds: pd.DataFrame):
 
     bag = bag.merge(
-        bonds[["ISIN", "pay_date", "pay_val", "month_end"]],
+        bonds[["ISIN", "pay_date", "pay_val", "month_end", "type"]],
         how="left",
         on="ISIN",
     )
     bag["total_pay_val"] = bag["pay_val"] * bag["quantity"]
 
     return bag
+
+
+def analyse_bag(bag: pd.DataFrame):
+    bag = bag.copy()
+
+    bag["profit before tax"] = bag["expected return"] - bag["expenditure"]
+    bag["profit after tax"] = bag["profit before tax"] * (1 - bag["tax"])
+    bag["profit per bond"] = bag["profit after tax"] / bag["quantity"]
+    bag["profitability"] = bag["profit after tax"] / bag["expenditure"] * 100
+
+    return bag
+
+
+def format_bag(bag: pd.DataFrame):
+
+    bag["expected return"] = bag.groupby("ISIN")["total_pay_val"].transform("sum")
+    bag = bag.drop(
+        columns=[
+            "pay_val",
+            "month_end",
+            "total_pay_val",
+        ]
+    )
+    bag = bag.sort_values(by="pay_date", ascending=True).drop_duplicates(
+        subset="ISIN", keep="last"
+    )
+
+    actual, historic = split_dataframe(bag)
+
+    actual = analyse_bag(actual)
+
+    actual["pay_date"] = pd.to_datetime(actual["pay_date"]).dt.strftime("%d-%m-%Y")
+
+    columns_to_round = [
+        "expenditure",
+        "expected return",
+        "profit before tax",
+        "profit after tax",
+        "profit per bond",
+        "profitability",
+    ]
+
+    actual.loc[:, columns_to_round] = actual[columns_to_round].round(2)
+
+    combined_bag = pd.concat([actual, historic], ignore_index=True, sort=False)
+
+    return combined_bag
--- src/bondstool/utils.py
+++ src/bondstool/utils.py
@@ -17,3 +17,22 @@ def truncate_past_dates(df: pd.DataFrame, date=None, date_col="pay_date"):
         date = datetime.today()
 
     return df.loc[df[date_col] >= date]
+
+
+def split_dataframe(df: pd.DataFrame, column="pay_date"):
+
+    condition = pd.isna(df[column])
+    df_satisfying = df[condition]
+    df_not_satisfying = df[~condition]
+
+    return df_not_satisfying, df_satisfying
+
+
+def get_styleby_condition(df: pd.DataFrame, column="type", condition=None):
+    style_conditional = []
+    for i, row in enumerate(df):
+        if row[column] is condition:
+            style_conditional.append(
+                {"if": {"row_index": i}, "backgroundColor": "rgb(237, 237, 237)"}
+            )
+    return style_conditional
