14f5955b434be4e3aabb09ad9afc59136ee4adc1
Bohdan Yakovenko <byakovenko@kse.org.ua>
2023-09-04 20:14:09 +0300

    added verification of excel file

--- src/bondstool/data/bag.py
+++ src/bondstool/data/bag.py
@@ -1,28 +1,59 @@
+import sys
+
+import numpy as np
 import pandas as pd
 from bondstool.utils import MAP_HEADINGS, split_dataframe
 
-OVDP_BAG_PATH = "data/ovdp_bag_31.05.23.xlsx"
+OVDP_BAG_PATH = "data/ovdp_input_data.xlsx"
 ISIN_PREFIX = "UA4000"
+
+
+def verify_excel_file(file_path):
+    try:
+        df = pd.read_excel(file_path)
+
+        expected_columns = [
+            "ISIN",
+            "Кілть в портфелі",
+            "Загальна сума придбання",
+            "Податок на прибуток ЮО (ПнПр)",
+        ]
+        missing_columns = set(expected_columns) - set(df.columns)
+
+        if missing_columns:
+            sys.exit(
+                f"Warning: The Excel file is missing the following columns: "
+                f"{', '.join(missing_columns)}"
+            )
+
+        if df.isna().any().any():
+            sys.exit("Warning: The Excel file contains empty cells.")
+
+        expected_types = [object, np.int64, (np.int64, float), (np.int64, float)]
+
+        for column_name, expected_type in zip(expected_columns, expected_types):
+            column_type = df[column_name].dtype
+
+            if not isinstance(expected_type, tuple):
+                expected_type = (expected_type,)
+
+            if column_type not in expected_type:
+                sys.exit(f"Warning: Column '{column_name}' has incorrect data type.")
+
+        return df
+
+    except pd.errors.ParserError:
+        sys.exit("Error: The Excel file is not in the correct format.")
+    except Exception as e:
+        sys.exit(f"An error occurred: {e}")
 
 
 def read_bag_info():
 
-    bag = pd.read_excel(OVDP_BAG_PATH)
-    bag = bag.loc[1:10].reset_index(drop=True)
-    # bag["ISIN"] = ISIN_PREFIX + bag["ISIN"].astype("str")
-
-    bag = bag.drop(
-        columns=[
-            "Тип",
-            "Дата погашеня",
-            "Очікувана сума погашення",
-            "Інвестиційний результат очікуваний без податку на прибуток",
-            "Інвестиційний результат очікуваний за мінусом ПнПр",
-        ]
-    )
+    bag = verify_excel_file(OVDP_BAG_PATH)
 
     map_headings = {
-        "Кіл-ть в портфелі": "quantity",
+        "Кілть в портфелі": "quantity",
         "Загальна сума придбання": "expenditure",
         "Податок на прибуток ЮО (ПнПр)": "tax",
     }
