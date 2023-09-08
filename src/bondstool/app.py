4830e7655b7c7ce88c6f7314f94264f8770f2375
Bohdan Yakovenko <byakovenko@kse.org.ua>
2023-09-08 16:02:09 +0300

    attempt to implement ddc.store

--- src/bondstool/app.py
+++ src/bondstool/app.py
@@ -1,6 +1,6 @@
 import base64
 import io
-
+import json
 import dash
 import numpy as np
 import pandas as pd
@@ -51,8 +51,8 @@ raw_bonds = add_exchange_rates(raw_bonds, exchange_rates)
 bonds = normalize_payments(raw_bonds)
 bonds = calculate_profitability(bonds)
 
-bag = read_bag_info()
-bag = merge_bonds_info(bag, bonds)
+#bag = read_bag_info()
+#bag = merge_bonds_info(bag, bonds)
 
 doc_url, auc_date = get_doc_url_date()
 
@@ -60,15 +60,15 @@ isin_df = parse_xml_isins(get_auction_xml(doc_url))
 
 trading_bonds = filter_trading_bonds(isin_df, bonds)
 
-payment_schedule = get_payment_schedule(bag)
-formatted_bag = format_bag(bag)
+#payment_schedule = get_payment_schedule(bag)
+#formatted_bag = format_bag(bag)
 
-monthly_bag = payments_by_month(bag)
-monthly_bag = fill_missing_months(monthly_bag)
+#monthly_bag = payments_by_month(bag)
+#monthly_bag = fill_missing_months(monthly_bag)
 
-recommended_bonds = get_recommended_bonds(bonds, monthly_bag)
+#recommended_bonds = get_recommended_bonds(bonds, monthly_bag)
 
-base_fig = make_base_monthly_payments_fig(monthly_bag)
+#base_fig = make_base_monthly_payments_fig(monthly_bag)
 
 
 app = Dash(__name__)
@@ -76,7 +76,7 @@ app = Dash(__name__)
 SLIDER_STEPS = np.arange(0, 5000, 200)
 
 
-def create_slider(id):
+def create_slider(id, recommended_bonds):
     if id in recommended_bonds["ISIN"].values:
         label_style = {"color": "red"}
     else:
@@ -97,7 +97,7 @@ def create_slider(id):
     )
 
 
-sliders = [create_slider(isin) for isin in isin_df["ISIN"].values]
+#sliders = [create_slider(isin) for isin in isin_df["ISIN"].values]
 
 
 app.layout = html.Div(
@@ -171,7 +171,7 @@ app.layout = html.Div(
                 ],
                 style={"display": "flex", "justify-content": "flex-end"},
             ),
-            *sliders,
+            html.Div(id="sliders"),
             dcc.Input(id="search-input", type="text", placeholder="Введіть ISIN"),
             html.Button("▼", id="dropdown-button"),
             dcc.Dropdown(
@@ -241,6 +241,13 @@ app.layout = html.Div(
                 },
             ),
             dcc.Download(id="download-dataframe-xlsx"),
+
+            dcc.Store(id='intermediate-bag'),
+            dcc.Store(id="intermediate-payment-schedule"),
+            dcc.Store(id="intermediate-formatted-bag"),
+            dcc.Store(id="intermediate-monthly-bag"),
+            dcc.Store(id="intermediate-recommended-bonds"),
+            dcc.Store(id="intermediate-base-fig")
         ]
     )
 
@@ -297,58 +304,108 @@ sliders_input = [Input(isin, "value") for isin in isin_df["ISIN"].values]
 uploaded_data = None
 
 
+@callback(Output('intermediate-bag', 'data'))
+def get_bag(_):
+
+    bag = read_bag_info()
+    bag = merge_bonds_info(bag, bonds)
+
+    return bag.to_json(date_format='iso', orient='split')
+
+
+@callback(
+    [Output("intermediate-payment-schedule", 'data'),
+     Output("intermediate-formatted-bag", 'data'),
+     Output("intermediate-monthly-bag", 'data')],
+    Input("intermediate-monthly-bag", 'data')
+    )
+def get_bag_derivatives(data):
+
+    bag = json.loads(data)
+
+    payment_schedule = get_payment_schedule(bag)
+
+    formatted_bag = format_bag(bag)
+
+    monthly_bag = payments_by_month(bag)
+    monthly_bag = fill_missing_months(monthly_bag)
+
+    return (payment_schedule.to_json(date_format='iso', orient='split'),
+            formatted_bag.to_json(date_format='iso', orient='split'),
+            monthly_bag.to_json(date_format='iso', orient='split'))
+
+
+@callback(
+    [Output("intermediate-recommended-bonds", 'data'),
+     Output("intermediate-base-fig", 'data')],
+    Input('intermediate-bag', 'data')
+    )
+def get_monthly_bag_derivatives(data):
+
+    monthly_bag = json.loads(data)
+
+    recommended_bonds = get_recommended_bonds(bonds, monthly_bag)
+
+    base_fig = make_base_monthly_payments_fig(monthly_bag)
+
+    return (recommended_bonds.to_json(date_format='iso', orient='split'),
+            base_fig.to_json(date_format='iso', orient='split'))
+
+
+
 @app.callback(
     [Output("output-data-upload", "children"),
-     Output("bag-table", "columns"),
-     Output("bag-table", "data"),
-     Output("bag-table", "style_data_conditional"),
-     Output("payment-schedule", "columns"),
-     Output("payment-schedule", "data"),
-     Output("graph-with-slider", "figure", allow_duplicate=True),
-     Output("dropdown", "options"),
-     Output("dropdown", "style", allow_duplicate=True)],
+     Output('intermediate-bag', "data")],
     [Input("upload-data", "contents")],
     [State("upload-data", "filename")],
     prevent_initial_call=True
 )
 def update_data_and_objects(contents, filename):
-    global bag, payment_schedule, formatted_bag, monthly_bag, recommended_bonds, base_fig, sliders
 
     if contents is None:
         raise PreventUpdate
 
-    formatted_bag, payment_schedule, recommended_bonds, base_fig, sliders = parse_contents(contents, filename)
+    _, data = contents.split(',')
 
-    json_formatted_bag = formatted_bag.to_dict('records')
-    json_payment_schedule = payment_schedule.to_dict('records')
+    padding = '=' * (4 - (len(data) % 4))
+    decoded_data = base64.b64decode(data + padding)
+
+    bag = pd.read_excel(io.BytesIO(decoded_data))
+    bag = pd.DataFrame(bag)
+    map_headings = {
+        "Кілть в портфелі": "quantity",
+        "Загальна сума придбання": "expenditure",
+        "Податок на прибуток ЮО (ПнПр)": "tax",
+    }
+
+    bag = bag.rename(columns=map_headings)
+    bag = merge_bonds_info(bag, bonds)
+
+    return bag.to_json(date_format='iso', orient='split')
 
 
-    style_data_conditional = get_style_by_condition(formatted_bag)
-
-    dropdown_options = [
-        {
-            "label": "Рекомендовані облігації",
-            "value": "Рекомендовані облігації",
-        }
-    ] + [{"label": isin, "value": isin} for isin in raw_bonds["ISIN"]]
-
-    dropdown_style = {"display": "none"} if not dropdown_options else {}
-
-    return (
-        html.Div(["File uploaded successfully."]),
-        [{"name": col, "id": col} for col in json_formatted_bag[0].keys()],
-        json_formatted_bag,
-        style_data_conditional,
-        [{"name": col, "id": col} for col in json_payment_schedule[0].keys()],
-        json_payment_schedule,
-        base_fig,
-        dropdown_options,
-        dropdown_style,
+@callback(
+    Output("sliders", 'data'),
+    Input('intermediate-recommended-bonds', 'data')
     )
+def get_sliders(data):
+
+    recommended_bonds = json.loads(data)
+
+    sliders = [create_slider(isin, recommended_bonds) for isin in isin_df["ISIN"].values]
+
+    return sliders
 
     
-@callback(Output("graph-with-slider", "figure"), *sliders_input)
-def update_figure(*amounts):
+@callback(
+    [Output("graph-with-slider", "figure"), *sliders_input],
+    [Input("intermediate-base-fig", "data"),
+    Input("intermediate-monthly-bag", "data")],
+    )
+def update_figure(base_fig_data, monthly_bag_data, *amounts):
+
+    base_fig = json.loads(base_fig_data)
+    monthly_bag = json.loads(monthly_bag_data)
 
     potential_payments = calc_potential_payments(
         trading_bonds, amounts, monthly_bag, isin_df
@@ -363,9 +420,14 @@ def update_figure(*amounts):
 
 @callback(
     Output("search-output", "children"),
-    [Input("search-input", "value"), Input("dropdown", "value")],
+    [Input("search-input", "value"),
+     Input("dropdown", "value"),
+     Input("intermediate-recommended-bonds", "data")],
 )
-def update_search_output(input_value, selected_option):
+def update_search_output(input_value, selected_option, recommended_bonds_data):
+
+    recommended_bonds = json.load(recommended_bonds_data)
+
     if selected_option == "Рекомендовані облігації":
         df = recommended_bonds
     elif input_value or selected_option:
@@ -413,10 +475,15 @@ def toggle_dropdown(n_clicks):
 
 @callback(
     Output("download-dataframe-xlsx", "data"),
-    Input("btn_xlsx", "n_clicks"),
+    [Input("btn_xlsx", "n_clicks"),
+     Input("intermediate-formatted-bag", 'data'),
+     Input("intermediate-payment-schedule", 'data')],
     prevent_initial_call=True,
 )
-def download_xlsx(n_clicks):
+def download_xlsx(n_clicks, formatted_bag_data, payment_schedule_data):
+
+    formatted_bag = json.load(formatted_bag_data)
+    payment_schedule = json.load(payment_schedule_data)
 
     xlsx_bytes = get_xlsx(formatted_bag, payment_schedule)
 
